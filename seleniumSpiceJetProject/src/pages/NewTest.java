0a20e490f2ec566d4602143ace21b8d7706d148b
venkatraman-r <rvenkatraman.cse@gmail.com>
2018-11-21 15:23:01 +0530

    SpiceJet TestCommit

---
+++ seleniumSpiceJetProject/src/pages/NewTest.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+	
+	WebDriver driver;
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  
+	  driver = new FirefoxDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://www.spicejet.com/");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
